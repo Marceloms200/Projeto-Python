b4bfbf5d02a53a73748cdac74af7202311f89c2d
Marcelo Machado de Souza <marcelo-souza@TUDOFACIL.INTRA.RS.GOV.BR>
2025-08-07 09:51:50 -0300

    first commit

---
+++ Projeto 1.py
@@ -0,0 +1,6 @@
+nome = input("Qual é o seu nome?")
+nascimento = int(input("Em que ano você nasceu?"))
+faculdade = input("Em qual instituição você estuda?")
+curso = input("Qual o seria o curso que está realizando neste momento?")
+idade = 2025 - nascimento
+print(f"Olá {nome.capitalize()},você fará {idade} anos de idade, está realizando a faculdade na {faculdade.capitalize()} realizando curso de {curso}.")
